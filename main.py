ed64c2099a4d8b27af2a791c2ae8e02f83339242
HARI RAJA <harihraja@gmail.com>
2018-05-30 15:46:25 -0700

    ver 0.01; one park, friday search

---
+++ main.py
@@ -0,0 +1,77 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START app]
+import logging
+
+import flask
+
+# [START imports]
+from datetime import datetime, timedelta, date
+
+import requests
+import requests_toolbelt.adapters.appengine
+
+# Use the App Engine Requests adapter. This makes sure that Requests uses
+# URLFetch.
+requests_toolbelt.adapters.appengine.monkeypatch()
+# [END imports]
+
+app = flask.Flask(__name__)
+
+PARKS = [{'id':'72393'}]
+SITE_URL = 'https://www.recreation.gov'
+CODE_PARAM = '&contractCode=NRSO'
+
+def getDate():
+    # next fri while (Mon-Sun: 0-6)
+    today = date.today()
+    friday = today + timedelta( (4-today.weekday()) % 7 )
+    return friday.strftime('%m/%d/%Y')
+
+def getCalendarURL(park_id, date):
+    ACTION_URL = '/campsiteCalendar.do?'
+    VIEW_PARAMS = 'page=calendar' + CODE_PARAM
+
+    url = SITE_URL + ACTION_URL + VIEW_PARAMS
+    url = url + '&parkId=' + park_id
+    url = url + '&calarvdate=' + date
+
+    return url
+
+def search(response_text):
+    return True
+
+@app.route('/')
+def index():
+    for park in PARKS:
+        # [START requests_get]
+        url = getCalendarURL(park['id'], getDate())
+        response = requests.get(url)
+        response.raise_for_status()
+        if search(response.text):
+            return response.text
+        # [END requests_get]
+
+    return flask.redirect(SITE_URL)
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+# [END app]
