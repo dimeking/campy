29cf4bdf24f19496a569b3594f3175c607f13ffd
HARI RAJA <harihraja@gmail.com>
2018-06-28 13:00:55 -0700

    ver 0.08, background refresh & quick display

--- campy_models.py
+++ campy_models.py
@@ -7,13 +7,17 @@ class UserInfo(ndb.Model):
     name = ndb.JsonProperty(required=True)
     preferences = ndb.JsonProperty(compressed=False)
 
-class TrackingInfo(ndb.Model):
+class PropertyInfo(ndb.Model):
+    id = ndb.StringProperty(required=True)
+    details = ndb.JsonProperty(compressed=False)
+    available_dates = ndb.JsonProperty(compressed=False)
+    
+class UserPropertyInfo(ndb.Model):
     email_id = ndb.StringProperty(required=True)
-    tracker_list = ndb.JsonProperty(compressed=False)
-    tracked_info = ndb.JsonProperty(compressed=False)
+    property_list = ndb.JsonProperty(compressed=False)
     
 
-def store_user_info(email_id, name, preferences):
+def save_user_info(email_id, name, preferences):
     # do not store or update to empty if or refresh token
     if not email_id or not name or not preferences:
         return False
@@ -37,8 +41,8 @@ def store_user_info(email_id, name, preferences):
         # update if found and different
         update = False
         user = users[0]
-        if user.name != json.dumps(name):
-            user.name = json.dumps(name)
+        if user.name != name:
+            user.name = name
             update = True
         if user.preferences != preferences:
             user.preferences = preferences
@@ -49,7 +53,7 @@ def store_user_info(email_id, name, preferences):
             print "user name: ", user.name
             print "user preferences: ", user.preferences
     except Exception:
-        logging.exception('Exception occured during store_user_info.')
+        logging.exception('Exception occured during save_user_info.')
         return False
 
     return True
@@ -60,103 +64,156 @@ def get_user_info(email_id):
         users = UserInfo.query(UserInfo.email_id == email_id).fetch()
     except Exception:
         logging.exception('Exception occured during get_user_info.')
-        return None    
+        return {}    
 
-    return users[0].name, users[0].preferences if any(users) else None
+    return users[0].name, users[0].preferences if any(users) else {}
 
 
-def store_tracker_item(email_id, tracker_item):
+def save_property_details(property_id, property_details):
     # do not store or update to empty if or refresh token
-    if not email_id or not tracker_item:
+    if not property_id or not property_details:
         return False
 
-    print "tracker_item: ", tracker_item
     try:
-        # look for user id
-        trackers = TrackingInfo.query(TrackingInfo.email_id == email_id).fetch()
+        # look for property id
+        properties = PropertyInfo.query(PropertyInfo.id == property_id).fetch()
 
         # if no entry then create
-        if not any(trackers):
-            tracker = TrackingInfo()
-            tracker.email_id = email_id  
-            tracker_items = [tracker_item]   
-            tracker.tracker_list = tracker_items    
-            tracker.put()
-            print "email_id: ", email_id
-            print "tracker_list: ", tracker.tracker_list                          
+        if not any(properties):
+            prop = PropertyInfo()
+            prop.id = property_id            
+            prop.details = property_details   
+            prop.put()
+            print "property_details: ", prop.details
             return True
 
         # update if found and different
-        tracker = trackers[0]
-        tracker_items = tracker.tracker_list       
-        print "tracker_items: ", tracker_items        
-        matched_items = [item for item in tracker_items if tracker_item['id'] in item]
-        if not any(matched_items):
-            tracker_items.append(tracker_item)
-            tracker.tracker_list = tracker_items       
-            tracker.put()
-            print "tracker_list: ", tracker.tracker_list            
+        prop = properties[0]
+        if prop.details != property_details:
+            prop.details = property_details
+            prop.put()
+            print "property_details: ", prop.details
+
     except Exception:
-        logging.exception('Exception occured during store_tracker_item.')
+        logging.exception('Exception occured during save_property_details.')
         return False
 
     return True
 
-def get_tracker_list(email_id):
-    print "get_tracker_list::email_id: ", email_id
+def save_property_dates(property_id, available_dates):
+    # do not store or update to empty if or refresh token
+    if not property_id or not available_dates:
+        return False
+
+    try:
+        # look for property id
+        properties = PropertyInfo.query(PropertyInfo.id == property_id).fetch()
+
+        # if no entry then create
+        if not any(properties):
+            prop = PropertyInfo()
+            prop.id = property_id            
+            prop.available_dates = available_dates   
+            prop.put()
+            print "property_id: ", prop.id
+            print "available_dates: ", prop.available_dates
+            return True
+
+        # update if found and different
+        prop = properties[0]
+        if not prop.available_dates:
+            prop.available_dates = {}
+        for day in available_dates.keys():
+            if day not in prop.available_dates or not prop.available_dates[day]:
+                prop.available_dates[day] = [] 
+            if prop.available_dates[day] != available_dates[day]:
+                prop.available_dates[day] = available_dates[day]
+                prop.put()
+                print "property_id: ", prop.id
+                print "available_dates: ", prop.available_dates
+
+    except Exception:
+        logging.exception('Exception occured during save_property_dates.')
+        return False
+
+    return True
+
+def get_property_details(property_id):
+    try:
+        # look for property id
+        properties = PropertyInfo.query(PropertyInfo.id == property_id).fetch()
+    except Exception:
+        logging.exception('Exception occured during get_property_details.')
+        return {}    
+
+    return properties[0].details if any(properties) and properties[0].details else {}
+
+def get_properties(num=100):
+    try:
+        # look for property id
+        properties = PropertyInfo.query().fetch(num)
+    except Exception:
+        logging.exception('Exception occured during get_properties.')
+        return []    
+
+    return [prop.id for prop in properties]
+
+def get_property_available_dates(property_id):
+    try:
+        # look for property id
+        properties = PropertyInfo.query(PropertyInfo.id == property_id).fetch()
+    except Exception:
+        logging.exception('Exception occured during get_property_available_dates.')
+        return {}    
+
+    return properties[0].available_dates if any(properties) and properties[0].available_dates else {}
+
+
+def save_user_property(email_id, property_id):
+    # do not store or update to empty if or refresh token
+    if not email_id or not property_id:
+        return False
+
+    print "property_id: ", property_id
+    try:
+        # look for user id
+        properties = UserPropertyInfo.query(UserPropertyInfo.email_id == email_id).fetch()
+
+        # if no entry then create
+        if not any(properties):
+            prop = UserPropertyInfo()
+            prop.email_id = email_id  
+            prop.property_list = [property_id]    
+            prop.put()
+            print "email_id: ", email_id
+            print "property_list: ", prop.property_list                          
+            return True
+
+        # update if found and different
+        prop = properties[0]
+        if not prop.property_list:
+            prop.property_list = []      
+        if property_id not in prop.property_list:
+            prop.property_list.append(property_id)
+            prop.put()
+            print "email_id: ", email_id
+            print "property_list: ", prop.property_list            
+    except Exception:
+        logging.exception('Exception occured during save_user_property.')
+        return False
+
+    return True
+
+def get_user_properties(email_id):
+    print "get_user_properties::email_id: ", email_id
     
     try:
         # look for user id    
-        trackers = TrackingInfo.query(TrackingInfo.email_id == email_id).fetch()
+        properties = UserPropertyInfo.query(UserPropertyInfo.email_id == email_id).fetch()
     except Exception:
-        logging.exception('Exception occured during get_tracked_info.')
+        logging.exception('Exception occured during get_user_properties.')
         return []    
     
-    tracker_list = trackers[0].tracker_list if any(trackers) else []
-    print "get_tracker_list::tracker_list: ", tracker_list
-    return tracker_list
-    # return { 'email_id': trackers[0].email_id, 'tracker_list': json.load(trackers[0].tracker_list) } if any(trackers) else None
-
-def store_tracked_info(email_id, tracked_info):
-    # do not store or update to empty if or refresh token
-    if not email_id or not tracked_info:
-        return False
-
-    try:
-        # look for user id
-        trackers = TrackingInfo.query(TrackingInfo.email_id == email_id).fetch()
-
-        # if no entry then create
-        if not any(trackers):
-            tracker = TrackingInfo()
-            tracker.email_id = email_id  
-            tracker.tracked_info = tracked_info       
-            tracker.put()
-            print "tracked_info: ", tracker.tracked_info
-            
-            return True
-
-        # update if found and different
-        tracker = trackers[0]
-        if tracker.tracked_info != tracked_info:
-            tracker.tracked_info = tracked_info       
-            tracker.put()
-            # print "tracked_info: ", tracker.tracked_info
-            
-    except Exception:
-        logging.exception('Exception occured during store_tracked_info.')
-        return False
-
-    return True
-
-def get_tracked_info(email_id):
-    try:
-        # look for user id    
-        trackers = TrackingInfo.query(TrackingInfo.email_id == email_id).fetch()
-    except Exception:
-        logging.exception('Exception occured during get_tracked_info.')
-        return None    
-
-    return trackers[0].tracked_info if any(trackers) else None
-    # return { 'email_id': trackers[0].email_id, 'tracked_info': json.load(trackers[0].tracked_info) } if any(trackers) else None
+    return properties[0].property_list if any(properties) and properties[0].property_list else []
+    # print "get_user_properties::property_list: ", property_list
 
--- main.py
+++ main.py
@@ -88,22 +88,24 @@ def getPropertyURL(id):
     return url
 
 TOP_PROPERTIES = [
-    {'id':'72393', 'name':'Point Reyes NSS', 'url':getPropertyURL('72393')}, 
-    {'id':'70926', 'name':'TUOLUMNE MEADOWS, Yosemite NP', 'url':getPropertyURL('70926')}, 
-    {'id':'70925', 'name':'UPPER PINES, Yosemite NP', 'url':getPropertyURL('70925')}, 
-    {'id':'70928', 'name':'LOWER PINES, Yosemite NP', 'url':getPropertyURL('70928')}, 
-    {'id':'70927', 'name':'NORTH PINES, Yosemite NP', 'url':getPropertyURL('70927')}, 
-    {'id':'71531', 'name':'FALLEN LEAF, Lake Tahoe', 'url':getPropertyURL('71531')}, 
-    {'id':'70980', 'name':'Scorpion, Channel Islands NP', 'url':getPropertyURL('70980')},
-    {'id':'73984', 'name':'Pinnacles NP', 'url':getPropertyURL('73984')}, 
-    {'id':'110457', 'name':'Point Pinole NP', 'url':getPropertyURL('110457')}, 
-    {'id':'110453', 'name':'Coyote Hills RP', 'url':getPropertyURL('110453')}, 
-    {'id':'110003', 'name':'Del Valle RP', 'url':getPropertyURL('110003')}, 
-    {'id':'110028', 'name':'Sunol RP', 'url':getPropertyURL('110028')}, 
-    {'id':'110452', 'name':'Black Diamond RP', 'url':getPropertyURL('110452')}, 
-    {'id':'1060800', 'name':'Clear Lake Campground', 'url':getPropertyURL('1060800')}, 
-    {'id':'1061750', 'name':'CAMPGROUND BY THE LAKE, South Lake Tahoe', 'url':getPropertyURL('1061750')}, 
+    {'id':'72393', 'name':'Point Reyes National Seashore Campground, CA', 'url':getPropertyURL('72393')}, 
+    {'id':'70926', 'name':'TUOLUMNE MEADOWS, CA', 'url':getPropertyURL('70926')}, 
+    {'id':'70925', 'name':'UPPER PINES, CA', 'url':getPropertyURL('70925')}, 
+    {'id':'70928', 'name':'LOWER PINES, CA', 'url':getPropertyURL('70928')}, 
+    {'id':'70927', 'name':'NORTH PINES, CA', 'url':getPropertyURL('70927')}, 
+    {'id':'71531', 'name':'FALLEN LEAF CAMPGROUND, CA', 'url':getPropertyURL('71531')}, 
+    {'id':'70980', 'name':'SANTA CRUZ SCORPION, CA', 'url':getPropertyURL('70980')},
+    {'id':'73984', 'name':'PINNACLES CAMPGROUND, CA', 'url':getPropertyURL('73984')}, 
+    {'id':'110457', 'name':'Point Pinole Regional Shoreline, CA', 'url':getPropertyURL('110457')}, 
+    {'id':'110453', 'name':'Coyote Hills Regional Park, CA', 'url':getPropertyURL('110453')}, 
+    {'id':'110003', 'name':'Del Valle, CA', 'url':getPropertyURL('110003')}, 
+    {'id':'110028', 'name':'Sunol, CA', 'url':getPropertyURL('110028')}, 
+    {'id':'110452', 'name':'Black Diamond Mines Regional Preserve, CA', 'url':getPropertyURL('110452')}, 
+    {'id':'1060800', 'name':'Clear Lake Campground, CA', 'url':getPropertyURL('1060800')}, 
+    {'id':'1061750', 'name':'CAMPGROUND BY THE LAKE, CA', 'url':getPropertyURL('1061750')}, 
      ]
+def top_property_list():
+    return [prop['id'] for prop in TOP_PROPERTIES]
 
 def search_property_name(response_text):
 
@@ -188,80 +190,139 @@ def get_property_details(tracker_url):
 
     return { 'id': id, 'name': name, 'url': tracker_url }
 
-def generate_tracked_info(properties, day):
+def generate_available_dates(property_id, day):
 
     # search for saturdays for 6 months
     dow, day = dayofweek(day)
     search_dates = getSearchDates(dow, 26)
 
     # search in all properties
-    tracked_info = properties
-    # tracked_info['timestamp'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    for prop in properties:
-        prop['available_dates'] = {} if 'available_dates' not in prop else prop['available_dates']
-        prop['available_dates'][day] = []
-        # prop['search dates'] = search_dates
-        print "property name: ", prop['name']
-        
-        for search_date in search_dates:
-            # print "search date: ", search_date
-
-            url = getCalendarURL(prop['id'], search_date)
+    timestamp = datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
+    property_available_dates = {}
+    property_available_dates[day] = {'timestamp':timestamp, 'dates':[]}
     
-            # [START requests_get]
-            response = requests.get(url)
-            response.raise_for_status()
-            # [END requests_get]
+    for search_date in search_dates:
+        # print "search date: ", search_date
 
-            available_dates = search_available_dates(response.text)
-            if not any(available_dates):
-                continue
+        url = getCalendarURL(property_id, search_date)
 
-            if search_date in available_dates:
-                # print "Eureka: ", search_date
-                prop['available_dates'][day].append({'date': search_date, 'url': url})
-                # send_approved_mail('{}@appspot.gserviceaccount.com'.format(
-                #     app_identity.get_application_id()), prop['name'], url, search_date)
+        # [START requests_get]
+        response = requests.get(url)
+        response.raise_for_status()
+        # [END requests_get]
+
+        available_dates = search_available_dates(response.text)
+        if not any(available_dates):
+            continue
+
+        if search_date in available_dates:
+            # print "Eureka: ", search_date
+            property_available_dates[day]['dates'].append({'date': search_date, 'url': url})
+            # send_approved_mail('{}@appspot.gserviceaccount.com'.format(
+            #     app_identity.get_application_id()), prop['name'], url, search_date)
 
 
-    return tracked_info
+    return property_available_dates
 
 def get_date(date_str):
     m, d, y = date_str.split('/')
     dt = date(int(y),int(m),int(d))
     return dt
 
-def resolve_tracked_info(tracked_info):
-    fri_dates = [get_date(dt['date']).toordinal() for dt in tracked_info['available_dates']['Fri']]
-    sat_dates = [get_date(dt['date']).toordinal() for dt in tracked_info['available_dates']['Sat']]
+# def resolve_tracked_info(tracked_info):
+#     fri_dates = [get_date(dt['date']).toordinal() for dt in tracked_info['available_dates']['Fri']]
+#     sat_dates = [get_date(dt['date']).toordinal() for dt in tracked_info['available_dates']['Sat']]
 
-    # pick Fridays where Sat is available
-    f_dates = [date.fromordinal(dt).strftime('%m/%d/%Y') for dt in fri_dates if dt+1 in sat_dates]
-    fr_dates = [dt for dt in tracked_info['available_dates']['Fri'] if dt['date'] not in f_dates]
-    tracked_info['available_dates']['Fri'] = fr_dates
+#     # pick Fridays where Sat is available
+#     f_dates = [date.fromordinal(dt).strftime('%m/%d/%Y') for dt in fri_dates if dt+1 in sat_dates]
+#     fr_dates = [dt for dt in tracked_info['available_dates']['Fri'] if dt['date'] not in f_dates]
+#     tracked_info['available_dates']['Fri'] = fr_dates
 
-    return tracked_info
+#     return tracked_info
 
+def get_availability(useremail=None):
+    # get all properties of user or top properties 
+    property_list = models.get_user_properties(useremail) if useremail else models.get_properties()
+    property_ids = property_list if any(property_list) else top_property_list()   
+    print "property_ids: ", property_ids    
+    
+    # show its availability
+    properties = []
+    for proprety_id in property_ids:
+        prop = models.get_property_details(proprety_id)
+        prop['available_dates'] = models.get_property_available_dates(proprety_id)
+        if not prop['available_dates']:
+            prop['available_dates']['Fri'] = {'timestamp':'', 'dates':[]}
+            prop['available_dates']['Sat'] = {'timestamp':'', 'dates':[]}
+
+        properties.append(prop)
+
+    return properties
+
+def update_property_availability(prop_id):
+    # save this property availability
+    fri_available_dates = generate_available_dates(prop_id, 'Fri')
+    models.save_property_dates(prop_id, fri_available_dates)
+    sat_available_dates = generate_available_dates(prop_id, 'Sat')
+    models.save_property_dates(prop_id, sat_available_dates)
+
+    prop = models.get_property_details(prop_id)
+    prop['available_dates'] = models.get_property_available_dates(prop_id)
+    return prop
+
+def update_availability(useremail=None):
+    # get all properties of user or top properties 
+    property_list = models.get_user_properties(email) if useremail else models.get_properties()
+    property_ids = property_list if any(property_list) else top_property_list()   
+    print "property_ids: ", property_ids    
+
+    properties = []
+    for property_id in property_ids:
+        prop = update_property_availability(property_id)
+        properties.append(prop)
+
+    return properties
+
+@app.route('/start')
+def start():
+    print "start: "
+    show = request.args.get('show') if 'show' in request.args else None
+
+    for property_details in TOP_PROPERTIES:
+        models.save_property_details(property_details['id'], property_details)
+
+    update_availability()
+    return flask.redirect(url_for('index')) if show=='True' else 'OK', 200
+
+
+@app.route('/refresh')
+def refresh():
+    print "refresh: "
+    useremail = request.args.get('useremail') if 'useremail' in request.args else None
+    show = request.args.get('show') if 'show' in request.args else None
+
+    # refresh availability 
+    # get all properties of user or top properties
+    update_availability(useremail)
+
+    return flask.redirect(url_for('index')) if show=='True' else 'OK', 200
 
 @app.route('/')
 def index():
 
-    day = request.args.get('dayofweek') if 'dayofweek' in request.args else None
-    days = request.args.get('days') if 'days' in request.args else None
+    # day = request.args.get('dayofweek') if 'dayofweek' in request.args else None
+    # days = request.args.get('days') if 'days' in request.args else None
     useremail = request.args.get('useremail') if 'useremail' in request.args else None
     
-    trackers = models.get_tracker_list(useremail) if useremail else []
-    properties = trackers if any(trackers) else TOP_PROPERTIES   
-    print "properties or trackers: ", properties    
-    tracked_info = generate_tracked_info(TOP_PROPERTIES, 'Fri')
-    tracked_info = generate_tracked_info(tracked_info, 'Sat')
-    # tracked_info = resolve_tracked_info(tracked_info)
+    # refresh availability if need be
+    # get all properties of user or top properties and show its availability
+    properties = get_availability(useremail)
 
     # [START render_template]
     return render_template(
         'submitted_form.html',
         username='Every One',
-        properties=tracked_info)
+        properties=properties)
     # [END render_template]
 
 # [START form]
@@ -280,25 +341,24 @@ def submitted_form():
     frequency = request.form['frequency']
     alerts = request.form['alerts']
 
-    models.store_user_info(email, {"name" : username}, {"frequency": frequency, "alerts": alerts})
+    # save this property details
+    models.save_user_info(email, {"name" : username}, {"frequency": frequency, "alerts": alerts})
     property_details = get_property_details(tracker_url)
-    models.store_tracker_item(email, property_details)
+    models.save_property_details(property_details['id'], property_details)
+    models.save_user_property(email, property_details['id'])
 
-    trackers = models.get_tracker_list(email) 
-    properties = trackers if any(trackers) else TOP_PROPERTIES   
-    print "properties or trackers: ", properties    
-    tracked_info = generate_tracked_info(properties, 'Fri')
-    tracked_info = generate_tracked_info(tracked_info, 'Sat')
-    # tracked_info = resolve_tracked_info(tracked_info)
-    
-    # models.store_tracked_info(email, tracked_info)
+    # refresh this property availability
+    update_property_availability(property_details['id'])
+
+     # get all properties of user or top properties and show its availability
+    properties = get_availability(email)   
 
     # [END submitted]
     # [START render_template]
     return render_template(
         'submitted_form.html',
         username=username,
-        properties=tracked_info)
+        properties=properties)
     # [END render_template]
 
 @app.errorhandler(500)
